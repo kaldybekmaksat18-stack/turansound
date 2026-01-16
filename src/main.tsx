920f365c7f2dffc5f72d65ed6c68a9ab642c63b8
kaldybekmaksat18-stack <kaldybekmaksat18@gmail.com>
2026-01-16 12:23:13 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/globals.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+// 🔴 ВАЖНО — ИМЕННО ЭТА СТРОКА
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
